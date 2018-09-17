277ed55bb51100ee75bdff3ad3acdb0ac96968af
bahram shah <bahram.rajabiws@gmail.com>
2018-09-17 18:22:29 +0430

    en language fixed

--- startup/db.js
+++ startup/db.js
@@ -4,5 +4,5 @@ const config = require('config');
 module.exports = function(){
     mongoose.connect(config.get('db.address'))
     .then(()=>{ console.log(`mongodb run at ${config.get('db.address')}`)})
-    .catch(()=> { console.log('mongoodb couldnt start'); process.exit(1); });
+    .catch(()=> { console.log("mongodb cannot connect."); process.exit(1); });
 }
