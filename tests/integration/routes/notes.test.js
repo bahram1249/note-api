08e950616880dc5504f9bd71e26d5ba0069b4331
bahram shah <bahram.rajabiws@gmail.com>
2018-09-17 18:26:48 +0430

    refactor code

--- tests/integration/routes/notes.test.js
+++ tests/integration/routes/notes.test.js
@@ -15,34 +15,45 @@ describe('/api/notes', ()=>{
         await server.close();
     });
 
+    // create a new test user
+    let token;
+    let userId;
+    async function createUser(){
+        userId = new mongoose.Types.ObjectId().toHexString();
+        const user = new User({
+            _id: userId,
+            name: 'bahram',
+            email: 'bahram.rajabiws@gmail.com',
+            password: '123456'
+        });
+        token = user.generateAuthToken();
+        await user.save()
+    }
+
+    //create a new test note
+    async function createNote(){
+        return await request(server)
+                .post('/api/notes')
+                .set('x-auth-token', token)
+                .send({
+                    title: 'test title',
+                    text: 'test text',
+                    dateReminder: Date.now()
+                });
+    }
+
     describe('/POST /api/notes', ()=>{
 
         it('Should create a note', async ()=>{
 
             // create a user first
-            const id = new mongoose.Types.ObjectId().toHexString();
-            let user = new User({
-                _id: id,
-                name: 'bahram',
-                email: 'bahram.rajabiws@gmail.com',
-                password: '123456'
-            });
-            const token = user.generateAuthToken();
-            await user.save();
-    
+            await createUser();
+
             // create a note
-            let note = {
-                title: 'test title',
-                text: 'test text',
-                dateReminder: Date.now()
-            }
-            note = await request(server)
-                .post('/api/notes')
-                .set('x-auth-token', token)
-                .send(note);
+            const note = await createNote();
             
             expect(note.status).toBe(200);
-            expect(note.body.user).toBe(id);
+            expect(note.body.user).toBe(userId);
             expect(note.body.dateReminder).toBeDefined();
             expect(note.body.title).toBeDefined();
             expect(note.body.text).toBeDefined();
@@ -50,14 +61,9 @@ describe('/api/notes', ()=>{
     
         it('Should not create note without login', async ()=>{
     
-            let note = {
-                title: 'test title',
-                text: 'test text',
-                dateReminder: Date.now()
-            }
-            note = await request(server)
-                .post('/api/notes')
-                .send(note);
+            // create a note
+            token = '';
+            const note = await createNote()
     
             expect(note.status).toBe(401);
         });
@@ -68,56 +74,87 @@ describe('/api/notes', ()=>{
         it('/GET with token', async ()=>{
 
             // create a user
-            const userId = new mongoose.Types.ObjectId().toHexString();
-            let user = new User({
-                _id: userId,
-                name: 'bahram',
-                email: 'bahram.rajabiws@gmail.com',
-                password: '123456'
-            });
-            const token = user.generateAuthToken();
-            await user.save();
+           await createUser();
     
             // create a note
-            const noteId = new mongoose.Types.ObjectId().toHexString();
-            let note = new Note({
-                _id: noteId,
-                title: 'test title',
-                text: 'test text',
-                dateReminder: Date.now(),
-                user: userId
-            });
-            await note.save();
+            const note = await createNote()
     
             const res = await request(server)
-                .get('/api/notes/' + noteId)
+                .get('/api/notes/' + note.body._id)
                 .set('x-auth-token', token);
     
             expect(res.status).toBe(200);
-            expect(res.body.text).toBe(note.text);
-            expect(res.body.title).toBe(note.title);
-            expect(new Date(res.body.dateReminder)).toMatchObject(note.dateReminder);
+            expect(res.body.text).toBe(note.body.text);
+            expect(res.body.title).toBe(note.body.title);
+            expect(new Date(res.body.dateReminder)).toMatchObject(new Date(note.body.dateReminder));
             expect(res.body.user).toBe(userId);
         });
 
         it('/GET without token', async ()=>{
 
-            // create note 
-            const userId = new mongoose.Types.ObjectId().toHexString();
-            const noteId = new mongoose.Types.ObjectId().toHexString();
-            let note = new Note({
-                _id: noteId,
-                title: 'test title',
-                text: 'test text',
-                dateReminder: Date.now(),
-                user: userId
-            });
-            await note.save();
+            // create a user
+            await createUser();
+            // create a note
+            const note = await createNote()
 
             const res = await request(server)
-                .get('/api/notes/' + noteId);
+                .get('/api/notes/' + note.body._id);
 
             expect(res.status).toBe(401);
         });
+
     });
+
+    describe('/DELETE /api/notes', ()=>{
+
+        it('/DELETE /api/notes', async ()=>{
+
+            // create user
+            await createUser();
+    
+            // create some note
+            let note = await createNote();
+            note = await createNote();
+            note = await createNote();
+            note = await createNote();
+
+            let notes = await Note.find({
+                user: userId
+            });
+
+            expect(notes.length).toBe(4);
+
+            const res = await request(server)
+                    .delete('/api/notes')
+                    .set('x-auth-token', token);
+    
+            notes = await Note.find({
+                user: userId
+            });
+
+            expect(res.status).toBe(200);
+            expect(notes.length).toBe(0);
+        });
+
+        it('/DELETE /api/notes/:id', async ()=>{
+
+            // create user
+            await createUser();
+    
+            // create note
+            const note = await createNote();
+
+            const res = await request(server)
+                    .delete('/api/notes/' + note.body._id)
+                    .set('x-auth-token', token);
+
+            expect(res.body.title).toBe(note.body.title);
+            expect(res.body.text).toBe(note.body.text);
+            expect(res.body.user).toBe(userId);
+            expect(res.status).toBe(200);
+        });
+    });
+
+
+
 });
