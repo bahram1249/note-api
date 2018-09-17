f43342a246eae86d04ccada95d6e20b30daaa2d5
bahram shah <bahram.rajabiws@gmail.com>
2018-09-17 14:09:23 +0430

    convert to clean code

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const config = require('config');
 const express = require('express');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const notes = require('./routes/notes');
 const app = express();
-const mongoose = require('mongoose');
 
 
-mongoose.connect(config.get('db.address'))
-    .then(()=>{ console.log(`mongodb run at ${config.get('db.address')}`)})
-    .catch(()=> { console.log('mongoodb couldnt start'); process.exit(1); });
-
-    
-app.use(express.json());
-app.use('/api/auth', auth);
-app.use('/api/users', users);
-app.use('/api/notes', notes);
+require('./startup/logging')();
+require('./startup/db')();
+require('./startup/routes')(app);
 
 const port = process.env.PORT || config.get('server.port');
 const server = app.listen(port, ()=>{
