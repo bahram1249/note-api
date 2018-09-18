7b6224ed29fa9d3f8ee5d5580110455547997524
bahram shah <bahram.rajabiws@gmail.com>
2018-09-19 03:29:04 +0430

    refactor code

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const config = require('config');
+const winston = require('winston');
 
-module.exports = function(){
-    mongoose.connect(config.get('db.address'))
-    .then(()=>{ console.log(`mongodb run at ${config.get('db.address')}`)})
-    .catch(()=> { console.log("mongodb cannot connect."); process.exit(1); });
+module.exports = async function(){
+    try{
+        await mongoose.connect(config.get('db.address'), { useNewUrlParser: true });
+        winston.log('info', `connected to mongodb on ${config.get('db.address')}`);
+    }
+    catch(ex) {
+        throw ex;
+    }
 }
