79d036ac6287e221b26f72f0be5248d8dbb5df2a
bahram shah <bahram.rajabiws@gmail.com>
2018-09-18 19:41:17 +0430

    refactor code

--- tests/integration/routes/auth.test.js
+++ tests/integration/routes/auth.test.js
@@ -14,15 +14,29 @@ describe('/api/auth routes', ()=>{
         await server.close();
     });
 
+    async function createUser(){
+        return await request(server)
+                        .post('/api/users')
+                        .send({
+                            name: 'bahram',
+                            email: 'bahram.rajabiws@gmail.com',
+                            password: '123456'
+                        });
+    }
+
+    async function auth(userLogin){
+        return await request(server)
+                        .post('/api/auth')
+                        .send(userLogin);
+    }
+
     it("Should be 400 if required body params not send it", async ()=>{
 
         const userLogin = {
             email: 'bahram.rajabiws@gmail.com'
         }
         
-        const res = await request(server)
-                            .post('/api/auth')
-                            .send(userLogin);
+        const res = await auth(userLogin);
 
         expect(res.status).toBe(400);
     });
@@ -34,22 +48,14 @@ describe('/api/auth routes', ()=>{
             password: '123456'
         }
         
-        const res = await request(server)
-                            .post('/api/auth')
-                            .send(userLogin);
+        const res = await auth(userLogin);
 
         expect(res.status).toBe(400);
     });
 
     it("Should be 400 if the password not equal", async ()=>{
 
-        let res = await request(server)
-                            .post('/api/users')
-                            .send({
-                                name: 'bahram',
-                                email: 'bahram.rajabiws@gmail.com',
-                                password: '123456'
-                            });
+        let res = await createUser();
         expect(res.status).toBe(200);
         
         
@@ -58,22 +64,14 @@ describe('/api/auth routes', ()=>{
             password: '1234567'
         }
         
-        res = await request(server)
-                            .post('/api/auth')
-                            .send(userLogin);
+        res = await auth(userLogin);
 
         expect(res.status).toBe(400);
     });
 
     it("Should be send it token", async ()=>{
 
-        let res = await request(server)
-                            .post('/api/users')
-                            .send({
-                                name: 'bahram',
-                                email: 'bahram.rajabiws@gmail.com',
-                                password: '123456'
-                            });
+        let res = await createUser();
         expect(res.status).toBe(200);
         
         
@@ -82,9 +80,7 @@ describe('/api/auth routes', ()=>{
             password: '123456'
         }
         
-        res = await request(server)
-                            .post('/api/auth')
-                            .send(userLogin);
+        res = await auth(userLogin);
 
         const token = res.get('x-auth-token');
         const decoded = jwt.verify(token, config.get('jsonwebtoken.privateKey'));
--- tests/integration/routes/users.test.js
+++ tests/integration/routes/users.test.js
@@ -14,55 +14,55 @@ describe('Users routes', ()=>{
         await server.close();
     });
 
+    let name = 'bahram';
+    async function createUser(){
+        const res = await request(server)
+                        .post('/api/users')
+                        .send({
+                            name,
+                            email: 'bahram.rajabiws@gmail.com',
+                            password: '123456'
+                        });
+        return res;
+    }
+
     it('Should create user successfully', async()=>{
 
-        const res = await request(server)
-            .post('/api/users')
-            .send({ name: 'bahran', email: 'bahram.rajabiws@gmail.com', password: '123456'});
+        const res = await createUser();
 
         expect(res.status).toBe(200);
     });
 
     it('Should status code be 400 if the user with this given email is existed before', async()=>{
 
-        const user = new User({
-            name: 'bahram',
-            email: 'bahram.rajabiws@gmail.com',
-            password: '123456'
-        });
-        await user.save();
+        await createUser();
 
-        const res = await request(server)
-            .post('/api/users')
-            .send({ name: 'bahram', email: 'bahram.rajabiws@gmail.com', password: '123456'});
+        const res = await createUser();
 
         expect(res.status).toBe(400);
     });
 
     it('Should status code be 400 if the body of request not correct', async()=>{
 
-        const res = await request(server)
-            .post('/api/users')
-            .send({ name: 'a', email: 'bahram.rajabiws@gmail.com', password: '123456'});
+        name = 'a';
+        const res = await createUser();
+
+        name = 'bahram';
 
         expect(res.status).toBe(400);
     });
 
     it('/GET /api/users/me', async()=>{
 
-        const user = new User({
-            name: 'bahram',
-            email: 'bahram.rajabiws@gmail.com',
-            password: '123456'
-        });
-        const token = user.generateAuthToken();
-        await user.save();
+        const user = await createUser();
+        const token = user.get('x-auth-token');
 
         const res = await request(server)
             .get('/api/users/me')
             .set('x-auth-token', token);
         
         expect(res.status).toBe(200);
+        expect(res.body._id).toBe(user.body._id);
     });
 
     it('/GET /api/users/me should be 400 if user deleted or deactived', async()=>{
@@ -85,17 +85,11 @@ describe('Users routes', ()=>{
     it('Should be hashed password', async ()=>{
         
         const password = '123456';
-        const res = await request(server)
-            .post('/api/users')
-            .send({
-                name: 'bahram',
-                email: 'bahram.rajabiws@gmail.com',
-                password
-            });
+        const res = await createUser();
 
         const user = await User.findById(res.body._id);
         const validPassword = bcrypt.compare(password, user.password);
         
         expect(validPassword).toBeTruthy();
     });
-})
+});
