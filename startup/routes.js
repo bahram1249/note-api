ccf6139c0523ab7b96719566b30d28bc5b907d95
bahram shah <bahram.rajabiws@gmail.com>
2018-09-22 02:15:41 +0330

    support x-form-encoded

--- startup/routes.js
+++ startup/routes.js
@@ -3,9 +3,11 @@ const users = require('../routes/users');
 const auth = require('../routes/auth');
 const notes = require('../routes/notes');
 const error = require('../middlewares/error');
+var bodyParser = require('body-parser');
 
 module.exports = function(app){
     app.use(express.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
     app.use('/api/auth', auth);
     app.use('/api/users', users);
     app.use('/api/notes', notes);
